25b10b9ceac3438cfda2d7966fbca390a5362e32
laur sandu <Sapistudio@users.noreply.github.com>
2018-07-27 18:37:54 +0300

    Create DmarcAnalyzer.php

---
+++ src/Analyzer/DmarcAnalyzer.php
@@ -0,0 +1,87 @@
+<?php
+namespace SapiStudio\DnsRecords\Analyzer;
+use SapiStudio\DnsRecords\Querifier;
+
+/**
+ * DmarcAnalyzer
+ * 
+ * @package 
+ * @copyright 2017
+ * @version $Id$
+ * @access public
+ */
+
+class DmarcAnalyzer
+{
+    protected $dmarcLine        = null;
+    protected $dmarcLineParsed  = [];
+    protected $dmarcTags        = [];
+    
+    /**
+     * DmarcAnalyzer::create()
+     * 
+     * @param mixed $dmarcLine
+     * @return
+     */
+    public static function create($dmarcLine = null)
+    {
+        return new static($dmarcLine);
+    }
+    
+    /**
+     * DmarcAnalyzer::__construct()
+     * 
+     * @param mixed $dmarcLine
+     * @return
+     */
+    public function __construct($dmarcLine){
+        if(!$dmarcLine)
+            throw new \InvalidArgumentException('A dmarc line is needed');
+        $this->loadDmarcTags();
+        $this->setDmarcLine($dmarcLine);
+        $this->parseDmarcLine();
+    }
+    
+    /**
+     * DmarcAnalyzer::loadDmarcTags()
+     * 
+     * @return
+     */
+    public function loadDmarcTags(){
+        $this->dmarcTags = include('../config/dmarcTags.php');;
+    }
+    
+    /**
+     * DmarcAnalyzer::parseDmarcLine()
+     * 
+     * @return
+     */
+    public function parseDmarcLine(){
+        $result = [];
+        array_walk(array_filter(array_map("trim",explode(';',$this->dmarcLine))), function($value,$key) use (&$result){
+            list($tag,$tagvalue) = explode("=", $value);
+            $result[$tag] = $tagvalue;
+});
+        $this->dmarcLineParsed = $result;
+        return $this;
+    }
+    
+    /**
+     * DmarcAnalyzer::getDmarcLine()
+     * 
+     * @return
+     */
+    public function getDmarcLine(){
+        return $this->dmarcLine;
+    }
+    
+    /**
+     * DmarcAnalyzer::setDmarcLine()
+     * 
+     * @param mixed $dmarcLine
+     * @return
+     */
+    public function setDmarcLine($dmarcLine){
+        $this->dmarcLine = $dmarcLine;
+    }
+}
