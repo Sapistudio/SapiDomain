15cda80c7574f21bf270404bcd186e325ab86e2e
laur sandu <Sapistudio@users.noreply.github.com>
2019-09-13 20:56:16 +0300

    Update DmarcAnalyzer.php

--- src/Analyzer/DmarcAnalyzer.php
+++ src/Analyzer/DmarcAnalyzer.php
@@ -12,6 +12,7 @@ class DmarcAnalyzer
     protected $dmarcLine        = null;
     protected $dmarcLineParsed  = [];
     protected $dmarcTags        = [];
+    protected $validDmarcLine   = false;
     
     /**
      * DmarcAnalyzer::create()
@@ -21,56 +22,50 @@ class DmarcAnalyzer
         return new static($dmarcLine);
     }
     
-    /**
-     * DmarcAnalyzer::__construct()
-     */
+    /** DmarcAnalyzer::__construct() */
     public function __construct($dmarcLine){
-        if(!$dmarcLine)
-            throw new \InvalidArgumentException('A dmarc line is needed');
         $this->loadDmarcTags();
         $this->setDmarcLine($dmarcLine);
-        $this->parseDmarcLine();
     }
     
-    /**
-     * DmarcAnalyzer::getDmarcPolicy()
-     */
+    /** DmarcAnalyzer::getDmarcPolicy() */
     public function getDmarcPolicy(){
         return $this->dmarcLineParsed->get('p','none');
     }
     
-    /**
-     * DmarcAnalyzer::loadDmarcTags()
-     */
+    /**  DmarcAnalyzer::loadDmarcTags() */
     public function loadDmarcTags(){
         $this->dmarcTags = include(dirname(__FILE__).'/../config/dmarcTags.php');
     }
     
-    /**
-     * DmarcAnalyzer::parseDmarcLine()
-     */
+    /** DmarcAnalyzer::dmarcIsValid() */
+    public function dmarcIsValid(){
+        return $this->validDmarcLine;
+    }
+    
+    /** DmarcAnalyzer::parseDmarcLine() */
     public function parseDmarcLine(){
-        $result     = [];
-        $dmarcLine  = array_filter(array_map("trim",explode(';',$this->dmarcLine)));
-        array_walk($dmarcLine, function($value,$key) use (&$result){
-            list($tag,$tagvalue) = explode("=", $value);
-            $result[$tag] = $tagvalue;
-});
-        $this->dmarcLineParsed = Collection::make($result);
+        if($this->dmarcLine && preg_match("/^v=dmarc(.*)/i", $this->dmarcLine)){
+            $result     = [];
+            $dmarcLine  = array_filter(array_map("trim",explode(';',$this->dmarcLine)));
+            array_walk($dmarcLine, function($value,$key) use (&$result){
+                list($tag,$tagvalue) = explode("=", $value);
+                $result[$tag] = $tagvalue;
+            });
+            $this->dmarcLineParsed  = Collection::make($result);
+            $this->validDmarcLine   = true;
+        }
         return $this;
     }
     
-    /**
-     * DmarcAnalyzer::getDmarcLine()
-     */
+    /** DmarcAnalyzer::getDmarcLine() */
     public function getDmarcLine(){
         return $this->dmarcLine;
     }
     
-    /**
-     * DmarcAnalyzer::setDmarcLine()
-     */
+    /** DmarcAnalyzer::setDmarcLine()*/
     public function setDmarcLine($dmarcLine){
         $this->dmarcLine = $dmarcLine;
+        $this->parseDmarcLine();
     }
 }
